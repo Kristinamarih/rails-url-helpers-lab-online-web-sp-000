8b97086ad85018b08d962fd28edde680ef33ab9b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-01 16:28:40 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,7 +18,7 @@ class StudentsController < ApplicationController
     end
     @student.active
     @student.save
-    redirect student_path(@student)
+    redirect_to student_path(@student)
   end
 
   private
